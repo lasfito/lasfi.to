541add69bfbac5e927a2aa13002f610cb01647f3
Adrian <79036345+lasfito@users.noreply.github.com>
2024-08-20 00:21:16 +0900

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -2,10 +2,10 @@ export const SITE_TITLE = "Lasfito";
 export const SITE_DESCRIPTION = "¡Web Dev sin relleno!";
 
 export const SHORT_RESUME_EN =
-  "Software Engineer with 3+ years of experience in web development (frontend / backend) and mobile development. Proficient in JavaScript, TypeScript, Node, React, and React Native. Currently learning Go, working on building an indie SaaS, and creating Spanish dev tutorials on YouTube.";
+  "Software Engineer with over 4 years of experience in web development (frontend and backend) and mobile development. Proficient in JavaScript, TypeScript, Node.js, React, and React Native.";
 
 export const SHORT_RESUME_ES =
-  "Ingeniero de Software con 3+ años de experiencia en desarrollo web (frontend / backend) y desarrollo móvil. Dominio en JavaScript, TypeScript, Node, React y React Native. Actualmente aprendiendo Go, trabajando en construir un proyecto SaaS y creando tutoriales en español sobre desarrollo web.";
+  "Ingeniero de software con más de 4 años de experiencia en desarrollo web (frontend y backend) y desarrollo móvil. Experto en JavaScript, TypeScript, Node.js, React y React Native.";
 
 export const GITHUB_URL = "https://github.com/lasfito/lasfi.to";
 
